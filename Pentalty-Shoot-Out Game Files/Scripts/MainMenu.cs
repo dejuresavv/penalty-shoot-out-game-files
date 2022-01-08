8dc8062e9001d8013ea310579e8f694785206907
dejuresavv <90507902+dejuresavv@users.noreply.github.com>
2022-01-08 15:08:54 +0200

    Update MainMenu.cs

--- Pentalty-Shoot-Out Game Files/Scripts/MainMenu.cs
+++ Pentalty-Shoot-Out Game Files/Scripts/MainMenu.cs
@@ -9,12 +9,16 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame() //Η PlayGame() καλειται καθε φορα που πατιεται το κουμπι PLAY του αρχικου μενου.
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); /* //ενας διαφορετικος τροπος απο το να εκτελεστει "SceneManager.LoadScene("Game");"
+                                                                              η συγκεκριμενη εντολη δουλευει αναλογα με το τι δεικτη εχουμε ορισει στο build settings
+                                                                               του unity, για καθε σκηνη. Η πρωτη σκηνη ειναι το κεντρικο μενου με δεικτη 0, η δευτερη
+                                                                               ειναι το παιχνιδι με δεικτη 1 και το τελικο μενου με δεικτη 2. Η συγκεκριμενη εντολη
+                                                                                παντα προσθετει +1 στο 0, οποτε παντα μεταφερεται στη σκηνη του παιχνιδιου.*/
     }
 
-    public void QuitGame ()
+    public void QuitGame ()//καλειται καθε φορα που πατιεται το κουμπι quit
     {
-        Debug.Log("i did quit!");
-        Application.Quit();
+        Debug.Log("i did quit!");//ενα απλο μηνυμα debug για να επιβεβαιωθει η λειτουργια του κουμπιου καθως φτιαχνοτανε
+        Application.Quit();//κλεινει την εφαρμογη.
     }
 }
