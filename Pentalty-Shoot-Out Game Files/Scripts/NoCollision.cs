2872951725754324b10ea9bf6976329c1ed838df
dejuresavv <90507902+dejuresavv@users.noreply.github.com>
2022-01-08 15:25:44 +0200

    Update NoCollision.cs

--- Pentalty-Shoot-Out Game Files/Scripts/NoCollision.cs
+++ Pentalty-Shoot-Out Game Files/Scripts/NoCollision.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class NoCollision : MonoBehaviour
 {
-    Rigidbody m_Rigidbody;
+    Rigidbody m_Rigidbody; // απαιτειται για να ορισουμε την βασικη εντολη για την οποια κατασκευαστηκε αυτο το script
     void Start()
     {
         
-        m_Rigidbody = GetComponent<Rigidbody>();
+        m_Rigidbody = GetComponent<Rigidbody>(); // απαιτειται για να ορισουμε την βασικη εντολη για την οποια κατασκευαστηκε αυτο το script
        
-        Physics.IgnoreLayerCollision(9, 10);
+        Physics.IgnoreLayerCollision(9, 10); //  H μπαλα και οι στοχοι στο τερμα διαθετουν απο ενα layer το οποιο εχει αριθμο 9 και 10 αντιστοιχα.
+                                             // Οταν αντιληφει το script οτι η μπαλα με τον στοχο εχουν συγκρουστει τοτε αποφασιζει να αγνοησει τη συγκρουση για
+                                             // να μπορεσει η μπαλα να καταληξει στα διχτυα. Χωρις αυτο το σκριπτ, οι στοχοι λειτουργουσαν σαν τερμαροφυλακες και απεκρουαν την μπαλα
     }
 }
