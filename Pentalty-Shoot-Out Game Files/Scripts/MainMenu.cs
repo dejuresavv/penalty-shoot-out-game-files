5cdbf65ef7a73f3d195ddbfefbe5be466b80a124
dejuresavv <90507902+dejuresavv@users.noreply.github.com>
2022-01-08 15:03:44 +0200

    Update MainMenu.cs

--- Pentalty-Shoot-Out Game Files/Scripts/MainMenu.cs
+++ Pentalty-Shoot-Out Game Files/Scripts/MainMenu.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
+//Το συγκεκριμενο σκριπτ τοποθετειται σε ενα GamePbject το οποιο περιεχει το κουμπι PLAY και το κουμπι QUIT του αρχικου μενου
+                            
 {
-    public void PlayGame()
+    public void PlayGame() //Η PlayGame() καλειται καθε φορα που πατιεται το κουμπι PLAY του αρχικου μενου.
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
