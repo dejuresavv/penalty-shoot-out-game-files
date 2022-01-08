d24fd6f35c9a6ceee975d2f6681bbf39799fd22c
dejuresavv <90507902+dejuresavv@users.noreply.github.com>
2022-01-08 15:22:18 +0200

    Update GoalkeeperDives.cs

--- Pentalty-Shoot-Out Game Files/Scripts/GoalkeeperDives.cs
+++ Pentalty-Shoot-Out Game Files/Scripts/GoalkeeperDives.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GoalkeeperDives : MonoBehaviour
+//Συνεργαζεται με το GKAnimController.controller
 {
     private Animator anim;
 
@@ -12,10 +13,12 @@ public class GoalkeeperDives : MonoBehaviour
     }
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))//καθε φορα που πατιεται αριστερο κλικ για να σουταρουμε την μπαλα σε ενα στοχο, ταυτοχρονα εκτελειται και
+                                        // μια απο τις 3 διαφορετικες κινησεις του τερματοφυλακα. Αρχικα ειναι σε idle μορφη και εχει 3 επιλογες κινησης
+                                        // οι οποιες βασιζονται σε τυχαιοτητα με την εντολη 1. 
         {
-            anim.SetInteger("DiveIndex", Random.Range(0, 3));
-            anim.SetTrigger("Dive");
+            anim.SetInteger("DiveIndex", Random.Range(0, 3));// ΕΝΤΟΛΗ 1
+            anim.SetTrigger("Dive"); // παιρνει τον τυχαιο δεικτη που διαλεξε απο την εντολη 1 και εκτελει το animation που του αντιστοιχει.
         }
     }
 
