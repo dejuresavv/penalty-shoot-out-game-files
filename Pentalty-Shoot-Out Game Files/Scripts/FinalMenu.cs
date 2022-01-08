2925ab9b6ff75e74988868bb2ab0190a60a92216
dejuresavv <90507902+dejuresavv@users.noreply.github.com>
2022-01-08 15:14:14 +0200

    Update FinalMenu.cs

--- Pentalty-Shoot-Out Game Files/Scripts/FinalMenu.cs
+++ Pentalty-Shoot-Out Game Files/Scripts/FinalMenu.cs
@@ -5,12 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class FinalMenu : MonoBehaviour
 {
-    public void PlayAgain()
+
+    //πανομοιοτυπη λογικη με το MainMenu.cs
+    public void PlayAgain()//καλειται καθε φορα που πατιεται το κουμπι PLAY AGAIN φορτωνει τη σκηνη του παιχνιδου
     {
         SceneManager.LoadScene("Game");
     }
 
-    public void QuitGame()
+    public void QuitGame() // καλειται καθε φορα που πατιεται το κουμπι QUIT και κλεινει τη εφαρμογη.
     {
         Debug.Log("Quit");
         Application.Quit();
