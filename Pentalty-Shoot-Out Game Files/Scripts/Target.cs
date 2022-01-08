abeb37cb76284a7b9fe4e4264f5a9a51b20060e6
dejuresavv <90507902+dejuresavv@users.noreply.github.com>
2022-01-08 16:42:07 +0200

    Update Target.cs

--- Pentalty-Shoot-Out Game Files/Scripts/Target.cs
+++ Pentalty-Shoot-Out Game Files/Scripts/Target.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    // ενα script περισσοτερο αισθητικης βελτιωσης UI του παιχνιδιου. Καθε φορα που ο κερσορας περναει πανω απο ενα στοχο, αλλαζει χρωμα για να υποδειξει το γεγονος
+    // οτι ειναι ετοιμος για να δεχτει τη μπαλα.
     private Renderer renderer;
         
     
     void Start()
     {
-        renderer = GetComponent<Renderer>(); 
+        renderer = GetComponent<Renderer>(); //μεσω της renderer ειναι εφικτο να αλλαζουμε χρωματα κατα τη διαρκεια του παιχνιδιου
     }
 
     void Update()
@@ -18,11 +20,13 @@ public class Target : MonoBehaviour
 
     private void OnMouseEnter()
     {
+    // γινεται μπλε οταν ο κερσορας ειναι πανω στον στοχο
         renderer.material.color = Color.blue;
     }
 
     private void OnMouseExit()
     {
+    // επιστρεφει στο κοκκινο χρωμα οταν ο κερσορας απομακρυνεται
         renderer.material.color = Color.red;
     }
 
